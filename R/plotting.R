26e21148441d3776d64fe012fec3009eeea2be58
mauraisa <aaron.maurais@bc.edu>
2023-12-29 17:41:14 -0500

    Add metadata df as argument for PCA plotting functions.

--- R/plotting.R
+++ R/plotting.R
@@ -11,6 +11,7 @@ getName <- function(vec) {
 #' Make PCA scatter plot.
 #' 
 #' @param pc A PCA matrix.
+#' @param dat.metadata A dataframe mapping replicate names to metadata terms to use in plots.
 #' @param color.col Column to use for color aesthetic.
 #' @param plot.title Plot title. By default there is no title.
 #' @param show.ylab Show y axis label?
@@ -21,10 +22,10 @@ getName <- function(vec) {
 #' 
 #' @import ggplot2
 #' @export
-PCAScatterPlot <- function(pc, color.col, plot.title=NULL,
+PCAScatterPlot <- function(pc, dat.metadata, color.col, plot.title=NULL,
                            show.ylab=T, show.xlab=T, show.legend=F)
 {
-    dat <- dplyr::left_join(pc[['pc']], dat.meta, by='replicate')
+    dat <- dplyr::left_join(pc[['pc']], dat.metadata, by='replicate')
 
     p <- ggplot(dat, aes(x=PC1, y=PC2, color=get(color.col))) +
         geom_point() +
@@ -46,7 +47,9 @@ PCAScatterPlot <- function(pc, color.col, plot.title=NULL,
     }
 
     # add viridis color scale if color.col is numeric
-    color_scale = ifelse(is.numeric(dat.meta[[color.col]]), viridis::scale_color_viridis, scale_color_discrete)
+    color_scale = ifelse(is.numeric(dat.metadata[[color.col]]),
+                         viridis::scale_color_viridis,
+                         scale_color_discrete)
     p <- p + color_scale(name=getName(color.col))
 
     if(!show.legend) { p <- p + guides(color='none') }
@@ -60,12 +63,13 @@ PCAScatterPlot <- function(pc, color.col, plot.title=NULL,
 #' @param pcs A list of PCA matrices.
 #' @param row.cols Variables to use for rows.
 #' @param color.cols Variables to use for columns.
+#' @param dat.metadata A dataframe mapping replicate names to metadata terms to use in plots.
 #' 
 #' @return A patchwork plot object
 #' 
 #' @import patchwork
 #' @export
-arrangePlots <- function(pcs, row.cols, color.cols)
+arrangePlots <- function(pcs, row.cols, color.cols, dat.metadata)
 {
     p <- NULL # initlize empty plot group
     for(color.i in 1:length(color.cols))
@@ -77,10 +81,12 @@ arrangePlots <- function(pcs, row.cols, color.cols)
             plot.title <- if(color.cols[color.i] == color.cols[1]) getName(row.cols[row.i]) else { NULL }
 
             if(is.null(p)) {
-                p <- PCAScatterPlot(pcs[[row.cols[row.i]]], color.cols[color.i], plot.title=plot.title,
+                p <- PCAScatterPlot(pcs[[row.cols[row.i]]], dat.metadata,
+                                    color.cols[color.i], plot.title=plot.title,
                                     show.xlab=show.xlab, show.legend=show.legend)
             } else {
-                p <- p + PCAScatterPlot(pcs[[row.cols[row.i]]], color.cols[color.i], plot.title=plot.title,
+                p <- p + PCAScatterPlot(pcs[[row.cols[row.i]]], dat.metadata,
+                                        color.cols[color.i], plot.title=plot.title,
                                         show.xlab=show.xlab, show.legend=show.legend)
             }
         }
