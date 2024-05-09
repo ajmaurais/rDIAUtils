298e1c55dc9466493c00d63074a4cafeb96c2de7
mauraisa <mauraisa@uw.edu>
2024-05-09 10:53:31 -0700

    Add readWideMetadata function.

--- R/dataCleaning.R
+++ R/dataCleaning.R
@@ -1,11 +1,11 @@
 
 #' Convert a date string into POSIX format
-#' 
+#'
 #' @param dat Dataframe
 #' @param acquiredTimeCol Name of column with with the UTC date to parse
-#' 
+#'
 #' @return Dataframe with acquiredTimeCol as an R POSIXlt object.
-#' 
+#'
 #' @export
 parseAcquiredTime <- function(d, acquiredTimeCol='acquiredTime') {
     date.cols <- c('date', 'h', 'm', 's', 'pam')
@@ -24,7 +24,7 @@ parseAcquiredTime <- function(d, acquiredTimeCol='acquiredTime') {
 }
 
 #' Set zeros to the minimum non-zero area in x.
-#' 
+#'
 #' @export
 zeroToMin <- function(x) {
     sele <- x == 0 | is.na(x)
@@ -39,7 +39,7 @@ zeroToMin <- function(x) {
 #' @param file.basename The basename of the tsv/Rda file.
 #' @param ext The file extension. Default is '.tsv'
 #' @param sep The deliminator. Default is '\\t'
-#' 
+#'
 #' @export
 readDataFile <- function(file.basename, ext='.tsv', sep='\t') {
     if(!file.exists(paste0(file.basename, '.Rda'))) {
@@ -69,7 +69,7 @@ readDirectLFQMatrix <- function(fname)
     {
         message(paste('Loading', paste0(fname, '.tsv')))
         dat <- read.csv(paste0(fname, '.tsv'), sep='\t')
-        
+
         dat.l <- dat %>% pivot_longer(!one_of(c('protein', 'ion')), names_to='replicate', values_to='area') %>%
             dplyr::rename(precursor='ion') # rename ion column because it annoys me
 
@@ -82,14 +82,14 @@ readDirectLFQMatrix <- function(fname)
 }
 
 #' Convert long dataframe to wide numeric matrix.
-#' 
+#'
 #' @param d Long formatted dataframe
 #' @param valuesFrom The name of the column with values that should be in the matrix.
 #' @param rowsName Name of the column to be row names in the matrix
 #' @param columnsName Name of the column to be column names in the matrix
-#' 
+#'
 #' @return A numeric matrix.
-#' 
+#'
 #' @import magrittr
 #' @export
 longToMatrix <- function(d, valuesFrom, rowsName, columnsName)
@@ -102,18 +102,61 @@ longToMatrix <- function(d, valuesFrom, rowsName, columnsName)
 }
 
 #' Convert long dataframe to wide dataframe.
-#' 
+#'
 #' @param d Long formatted dataframe
 #' @param valuesFrom The name of the column with values to be spread into columns.
 #' @param rowsName Name of the column(s) to keep for each row.
 #' @param columnsName Name of the column to be column names.
-#' 
+#'
 #' @return A wide format dataframe.
-#' 
+#'
 #' @import magrittr
 #' @export
 pivotLonger <- function(d, valuesFrom, rowsName, columnsName)
 {
     dplyr::select(d, all_of(c(valuesFrom, rowsName, columnsName))) %>%
         tidyr::pivot_wider(names_from=all_of(columnsName), values_from=all_of(valuesFrom))
+}
+
+#' Read metadata and replicates from batch database.
+#'
+#' Metadata is converted from long format used in database and converted to wide format.
+#' The values from the sampleMetadataTypes table are then used to convert each column
+#' into the appropiate date type.
+#'
+#' @param conn An initialized database connection
+#'
+#' @return Replicate and metadata dataframe
+#'
+#' @export
+readWideMetadata <- function(conn)
+{
+    dat.rep <- DBI::dbGetQuery(conn,
+                                'SELECT
+                                    r.replicate,
+                                    r.replicateId,
+                                    r.project,
+                                    r.ticArea,
+                                    r.acquiredRank
+                                FROM replicates r;')
+    dat.meta.l <- DBI::dbGetQuery(conn, 'SELECT * FROM sampleMetadata;')
+    meta.types <- DBI::dbGetQuery(conn, 'SELECT annotationKey, annotationType
+                                         FROM sampleMetadataTypes;')
+
+    meta.converters <- as.list(setNames(meta.types$annotationType,
+                                        meta.types$annotationKey))
+    meta.converters <- lapply(meta.converters, function(x) {
+        if(x == 'BOOL') { return(as.logical) }
+        if(x == 'INT') { return(as.integer) }
+        if(x == 'FLOAT') { return(as.numeric) }
+        if(x == 'STRING') { return(function(x) {x}) }
+        stop(paste(x, 'is an unknown variable type!'))
+    })
+
+    dat.meta.w <- tidyr::pivot_wider(dat.meta.l, names_from='annotationKey', values_from='annotationValue')
+    for(column in names(meta.converters)) {
+        dat.meta.w[[column]] <- meta.converters[[column]](dat.meta.w[[column]])
+    }
+
+    dplyr::left_join(dat.rep, dat.meta.w, by='replicateId')
 }
